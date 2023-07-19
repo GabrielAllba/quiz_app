9f33d29fd66f626e8d80145fe64aff10539cbd06
rielgbrl <rielgasy9@gmail.com>
2023-07-19 21:56:48 +0700

    add start screen

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -4,6 +4,36 @@ class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
   @override
   Widget build(context) {
-    return const Text('hello');
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'Learn flutter the fun way!',
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            child: const Text('Start Quiz'),
+          )
+        ],
+      ),
+    );
   }
 }
